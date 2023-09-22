c7b8a8ea74305e72f9bd4696a357095a1743f125
Muhammad Umar <109038058+Muhammadumar20104@users.noreply.github.com>
2023-09-22 15:32:42 +0500

    All APIs Are Working And Every Things Slove

--- routes/Order.js
+++ routes/Order.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const { createOrder, fetchOrdersByUser, deleteOrder, updateOrder } = require('../controller/Order');
- 
-const router = express.Router();
+const { createOrder, fetchOrdersByUser, deleteOrder, updateOrder,fetchAllOrders } = require('../controller/Order');
 
+const router = express.Router();
+//  /orders is already added in base path
 router.post('/', createOrder)
-      .get('/', fetchOrdersByUser)
-      .delete('/:id',deleteOrder)
-      .patch('/:id',updateOrder)
-      
-exports.router = router
+      .get('/user/:userId', fetchOrdersByUser)
+      .delete('/:id', deleteOrder)
+      .patch('/:id', updateOrder)
+      .get('/',fetchAllOrders)
+
+
+exports.router = router;
