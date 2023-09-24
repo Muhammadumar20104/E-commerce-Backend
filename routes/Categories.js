d07445d6e371bfb38cfd10be07321d747602c6f5
Muhammad Umar <109038058+Muhammadumar20104@users.noreply.github.com>
2023-09-24 13:44:42 +0500

    Using Token For Authentication

--- routes/Categories.js
+++ routes/Categories.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { fetchCategories, createCategory } = require('../controller/Category');
 
 const router = express.Router();
+//  /categories is already added in base path
+router.get('/', fetchCategories).post('/',createCategory)
 
-router.get('/',fetchCategories).post('/',createCategory)
-exports.router = router
+exports.router = router;
