7aa0b5a6387e1fc32a85cfb883df2ea5b3d46f1a
Muhammad Umar <109038058+Muhammadumar20104@users.noreply.github.com>
2023-09-26 11:59:59 +0500

    All Without Error Are Working

--- services/common.js
+++ services/common.js
@@ -1,9 +1,19 @@
 const passport = require('passport');
 
 exports.isAuth = (req, res, done) => {
-  return passport.authenticate('jwt')
+  return passport.authenticate('jwt');
 };
 
-exports.sanitizeUser = (user)=>{
-    return {id:user.id, role:user.role}
-}
+exports.sanitizeUser = (user) => {
+  return { id: user.id, role: user.role };
+};
+
+exports.cookieExtractor = function (req) {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['jwt'];
+  }
+  //TODO : this is temporary token for testing without cookie
+  token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MTAyNjJkMTA1ODMyMjQxMTFmNDUyMSIsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjk1NjcwNDA2fQ.ZGQtgevqtuT1EdNJt_qNw01CEwHSa7tGWJtgF7n6XOU"
+  return token;
+};
